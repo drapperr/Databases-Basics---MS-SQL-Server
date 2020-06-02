7821fa175a60edc7f84ac9107fcf9fcf0501274d
DraPpeRr <drapperr@users.noreply.github.com>
2020-06-02 14:01:07 +0300

    update

---
+++ Table Relations/09. Peaks in Rila.sql
@@ -0,0 +1,5 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT MountainRange, PeakName, Elevation FROM Peaks AS p
+	JOIN Mountains AS m ON p.MountainId = m.Id
+	WHERE MountainRange = 'Rila'
+	ORDER BY Elevation DESC
