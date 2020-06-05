4594c1661ccd733c51ec05da41c78a1de797a2cd
DraPpeRr <drapperr@users.noreply.github.com>
2020-06-05 15:23:55 +0300

    update

---
+++ Built-in Functions/13. Mix of Peak and River Names.sql
@@ -0,0 +1,3 @@
+SELECT PeakName, RiverName,LOWER(CONCAT(LEFT(P.PeakName, LEN(P.PeakName)-1), R.RiverName)) AS [Mix] FROM Peaks AS P
+	JOIN Rivers AS R ON RIGHT(P.PeakName, 1) = LEFT(R.RiverName, 1)
+	ORDER BY Mix
